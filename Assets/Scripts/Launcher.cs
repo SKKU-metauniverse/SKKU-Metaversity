4aa688edff4fc42229547014105e4d3b0c7b19e1
DongMinKwon <38908169+DongMinKwon@users.noreply.github.com>
2021-09-24 19:46:53 +0900

    Room Create, Join
    Create 버튼을 누르면 랜덤하게 방 코드가 생성되고 방을 만듦. 아직은 콘솔에서만 방 코드를 확인할 수 있음.
    
    방 코드를 inputfield에 입력하고 join해야 방에 접속할 수 있음.

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     [SerializeField]
     private byte numPlayers = 4;
+    [SerializeField]
+    private InputField inputField;
 
     string gameVersion = "1";
     bool isConnecting = false;
+    bool isCreate = false;
+
+    string roomName;
+
+    private static System.Random random = new System.Random((int)DateTime.Now.Ticks & 0x0000FFFF); //랜덤 시드값
 
     void Awake()
     {
@@ -33,6 +43,37 @@ public class Launcher : MonoBehaviourPunCallbacks
         
     }
 
+    public static string RandomString(int _nLength = 12)
+    {
+        const string strPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";  //문자 생성 풀
+        char[] chRandom = new char[_nLength];
+
+        for (int i = 0; i < _nLength; i++)
+        {
+            chRandom[i] = strPool[random.Next(strPool.Length)];
+        }
+        string strRet = new String(chRandom);   // char to string
+        return strRet;
+    }
+    public void Create()
+    {
+        isConnecting = true;
+        isCreate = true;
+
+        if (!PhotonNetwork.IsConnected)
+        {
+            // Connect Internet
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings(); //call OnConnectedToMaster
+        }
+        else
+        {
+            roomName = RandomString();
+            PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = numPlayers }, null);
+        }
+
+    }
+
     public void Connect()
     {
         isConnecting = true;
@@ -45,18 +86,27 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
         else
         {
-            PhotonNetwork.JoinOrCreateRoom("SKKU", new RoomOptions { MaxPlayers = numPlayers }, null);
+            string inputName = inputField.text;
+            PhotonNetwork.JoinRoom(inputName);
         }
 
     }
 
     public override void OnConnectedToMaster()
-    {
+    { 
         base.OnConnectedToMaster();
         Debug.Log("connection complete!");
         
-        if(isConnecting)
-            PhotonNetwork.JoinOrCreateRoom("SKKU", new RoomOptions { MaxPlayers = numPlayers }, null);
+        if(isConnecting && isCreate)
+        {
+            roomName = RandomString();
+            PhotonNetwork.CreateRoom(roomName, new RoomOptions { MaxPlayers = numPlayers }, null);
+        }
+        else if(isConnecting && !isCreate)
+        {
+            string inputName = inputField.text;
+            PhotonNetwork.JoinRoom(inputName);
+        }
     }
 
     public override void OnDisconnected(DisconnectCause cause)
@@ -69,6 +119,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         base.OnJoinedRoom();
         Debug.Log("Create Room!");
+        Debug.Log(roomName);
 
         if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
         {
