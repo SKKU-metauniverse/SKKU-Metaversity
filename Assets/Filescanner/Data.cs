55a4641e374fc7605642bda9e0493328d09096b7
jihooni0914 <81512592+jihooni0914@users.noreply.github.com>
2021-10-01 18:27:22 +0900

    filescanner(demo)

---
+++ Assets/Filescanner/Data.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum DataType { Directory = 0, File }
+
+public class Data : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] spriteIcons;
+
+    private Image imageIcon;
+    private TextMeshProUGUI textDataName;
+
+    private DataType dataType;
+
+    private string fileName;
+    public string FileName => fileName;
+
+    private int maxFileNameLength = 25;
+
+    public void Setup(string fileName, DataType dataType)
+    {
+        imageIcon = GetComponentInChildren<Image>();
+        textDataName = GetComponentInChildren<TextMeshProUGUI>();
+
+        this.fileName = fileName;
+        this.dataType = dataType;
+
+        imageIcon.sprite = spriteIcons[(int)this.dataType];
+
+        textDataName.text = this.fileName;
+
+        if (fileName.Length >= maxFileNameLength)
+        {
+            textDataName.text = fileName.Substring(0, maxFileNameLength);
+            textDataName.text += "..";
+        }
+
+        SetTextColor();
+    }
+
+
+    private void SetTextColor()
+    {
+        if (dataType == DataType.Directory)
+        {
+            textDataName.color = Color.yellow;
+        }
+        else
+        {
+            textDataName.color = Color.white;
+        }
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Filescanner/DirectoryController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class DirectoryController : MonoBehaviour
+{
+    private DirectoryInfo currentDirectory;
+    private DIrectorySpawner directorySpawner;
+
+    private void Awake()
+    {
+        Application.runInBackground = true;
+
+        directorySpawner = GetComponent<DIrectorySpawner>();
+        directorySpawner.Setup();
+
+        string desktopFoleder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        currentDirectory = new DirectoryInfo(desktopFoleder);
+
+        UpdateDirectory(currentDirectory);
+    }
+
+    private void UpdateDirectory(DirectoryInfo directory)
+    {
+        currentDirectory = directory;
+
+        directorySpawner.UpdateDirectory(currentDirectory);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
