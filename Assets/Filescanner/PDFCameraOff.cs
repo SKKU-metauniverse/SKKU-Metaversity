368961dcbaa5202becbf541f8a4aade5b719b702
jihooni0914 <81512592+jihooni0914@users.noreply.github.com>
2021-10-22 21:20:30 +0900

    1022
    asdf

---
+++ Assets/Filescanner/PDFCameraOff.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PDFCameraOff : MonoBehaviour
+{
+    public GameObject camera1;
+    private void Update()
+    {
+        if (camera1.activeSelf)
+        {
+            if (Input.GetButtonDown("Cancel"))
+                camera1.SetActive(false);
+        }
+    }
+}
