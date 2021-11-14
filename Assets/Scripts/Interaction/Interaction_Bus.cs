52db26c6aa761425837312a01774ac1cec43418e
Selee <33747749+Lijuet@users.noreply.github.com>
2021-11-14 15:38:26 +0900

    [1114] Add interaction in board and car
    Basic classroom에서 게시판 내용 패널 추가 완료
    Nature classroom에서 car에 경적 소리 인터랙션 추가 중

---
+++ Assets/Scripts/Interaction/Interaction_Bus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction_Bus : ObjectInteraction
+{
+    private AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+    public override void Interaction()
+    {
+        audioSource.Play();
+    }
+}
