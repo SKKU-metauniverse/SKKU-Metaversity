d6b773c0bd7d57790e97365f9b05e7fa87b235f6
jihooni0914 <dbwlgns8314@gmail.com>
2021-11-26 18:45:43 +0900

    일단스탑

---
+++ Assets/jihoon/Video/Interaction_VideoPlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Interaction_VideoPlay : ObjectInteraction
+{
+    [SerializeField]
+    private VideoPlayer vp;
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        outline = transform.GetComponent<Outline>();
+        
+        audioSource = gameObject.GetComponentInParent<AudioSource>();
+        vp = gameObject.GetComponentInParent<VideoPlayer>();
+    }
+
+    public override void Interaction()
+    {
+        vp.Play();
+        audioSource.Play();
+    }
+}
---
+++ Assets/jihoon/Video/VideoController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+    private bool isMuted = false;
+
+    public void videoPlay() 
+    {
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+
+    public void videoPause()
+    {
+        
+        videoPlayer.Pause();
+        audioSource.Pause();
+    }
+
+    public void videoStop()
+    {
+        videoPlayer.Stop();
+        audioSource.Stop();
+    }
+
+    public void videoVolume(float vol)
+    {
+        audioSource.volume = vol;
+
+    }
+
+    public void SetVideoMute()
+    {
+        if (!isMuted)
+        {
+            videoPlayer.SetDirectAudioMute(0, true);
+        }
+        else 
+        {
+            videoPlayer.SetDirectAudioMute(0, false);
+        }
+    }
+
+}
